60e65dd2c2176c40aa99c9f7edb76477993acce0
luca2 <luca2006.mu@gmail.com>
2025-11-27 20:03:05 +0100

    first commit

---
+++ src/main/java/io/papermc/voidWorld/VoidWorld.java
@@ -0,0 +1,17 @@
+package io.papermc.voidWorld;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VoidWorld extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
