0200477b55f279c07571e3e75b0456f14d5d7c48
luca2 <luca2006.mu@gmail.com>
2025-12-02 21:45:59 +0100

    removed datapack detection inlined instance constructor

--- src/main/java/io/papermc/voidWorld/VoidWorld.java
+++ src/main/java/io/papermc/voidWorld/VoidWorld.java
@@ -1,6 +1,5 @@
 package io.papermc.voidWorld;
 
-import io.papermc.paper.datapack.Datapack;
 import io.papermc.voidWorld.recipes.VWRecipeGenerator;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
@@ -12,33 +11,20 @@ public final class VoidWorld extends JavaPlugin {
         getLogger().info("VoidWorld loaded!");
     }
 
-    private VWOneBlockGenerator oneBlock;
-    private VWRecipeGenerator recipeGen;
-
     @Override
     public void onEnable() {
         getLogger().info("VoidWorld enabled!");
 
-        oneBlock = new VWOneBlockGenerator(this);
-        recipeGen = new VWRecipeGenerator(this);
-
         World world = Bukkit.getWorlds().getFirst();
+
+        VWOneBlockGenerator oneBlock = new VWOneBlockGenerator(this);
+        VWRecipeGenerator recipeGen = new VWRecipeGenerator(this);
+
 
         Bukkit.getPluginManager().registerEvents(oneBlock, this);
 
         oneBlock.setOneBlock(world);
         recipeGen.registerRecipes();
-
-        Datapack pack = this.getServer().getDatapackManager().getPack(getPluginMeta().getName() + "/provided");
-        if (pack != null) {
-            if (pack.isEnabled()) {
-                getLogger().info("The Datapack loaded successfully!");
-            } else {
-                getLogger().warning("The datapack failed to load.");
-            }
-        } else {
-            getLogger().warning("The datapack was not found.");
-        }
     }
 
     @Override
