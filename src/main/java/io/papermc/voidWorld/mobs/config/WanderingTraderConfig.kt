6b5bf7fa4e23bc206a8b99a80ce165a15a87a7ba
VoidDevLuca <luca2006.mu@gmail.com>
2026-01-12 18:44:45 +0100

    modified WanderingTraderConfig to read wTrades.json (only works for 1 cost item)

--- src/main/java/io/papermc/voidWorld/mobs/config/WanderingTraderConfig.kt
+++ src/main/java/io/papermc/voidWorld/mobs/config/WanderingTraderConfig.kt
@@ -1,7 +1,6 @@
 package io.papermc.voidWorld.mobs.config
 
-import org.bukkit.Material
-import org.bukkit.inventory.ItemStack
+import io.papermc.voidWorld.mobs.helper.RItemStackConfiguration
 import org.bukkit.inventory.MerchantRecipe
 import org.bukkit.plugin.java.JavaPlugin
 import org.spongepowered.configurate.ConfigurationNode
@@ -13,34 +12,33 @@ class WanderingTraderConfig(
   fun getTrades(): MutableList<MerchantRecipe> {
     val pool: MutableList<MerchantRecipe> = mutableListOf()
 
-    pool.add(
-      createTrade(
-        Material.DIRT,
-        1,
-        Material.DIRT,
-        1,
-      ),
-    )
+    val tradesNode = root.node("tradeList")
+
+    for (trade in tradesNode.childrenList()) {
+      val uses = trade.node("uses").getInt(0)
+      val maxUses = trade.node("maxUses").getInt(9999)
+
+      val costItemNode = trade.node("costItem")
+      val costAmount = trade.node("costAmount").getInt(1)
+
+      val costItemConf = RItemStackConfiguration.parseItem(costItemNode) ?: continue
+      val costItem = RItemStackConfiguration.build(costItemConf) ?: continue
+      costItem.amount = costAmount
+
+      val resultItemNode = trade.node("resultItem")
+      val resultAmount = trade.node("resultAmount").getInt(1)
+
+      val resultItemConf = RItemStackConfiguration.parseItem(resultItemNode) ?: continue
+      val resultItem = RItemStackConfiguration.build(resultItemConf) ?: continue
+      resultItem.amount = resultAmount
+
+      val recipe = MerchantRecipe(resultItem, uses, maxUses, false)
+      recipe.addIngredient(costItem)
+
+      pool.add(recipe)
+    }
 
     pool.shuffle()
-
     return pool
-  }
-
-  private fun createTrade(
-    costMat: Material,
-    cost: Int,
-    resultMat: Material,
-    resultAmount: Int,
-  ): MerchantRecipe {
-    val trade = MerchantRecipe(
-      ItemStack(resultMat, resultAmount),
-      20,
-    )
-
-    trade.addIngredient(ItemStack(costMat, cost))
-    trade.priceMultiplier = 0f
-
-    return trade
   }
 }
